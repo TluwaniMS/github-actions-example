8ccafa222bced83d53afeb5414fd6bea3afe0213
tluwaniMS <tluwani2020@gmail.com>
2024-02-03 12:04:58 +0200

    final-touches-to-js-triggers

--- node-scripts-trigger/script.js
+++ node-scripts-trigger/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+
+const customConfig = {
+  dictionaries: [adjectives, colors, animals],
+  separator: '-',
+  length: 3,
+};
+
+const randomName = uniqueNamesGenerator(customConfig);
+
+console.log(`The random push generated name is ${randomName}! :)`);
